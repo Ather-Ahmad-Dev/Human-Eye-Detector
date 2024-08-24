2c02d010ecc36e532b59120b42c61827feed0d20
Ather-Ahmad-Dev <atherahmadwork@gmail.com>
2024-08-24 00:22:44 -0700

    OpenCV initialized

---
+++ app/src/main/java/com/example/humaneyedetector3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.humaneyedetector3;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.humaneyedetector3.databinding.ActivityMainBinding;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (OpenCVLoader.initLocal()){
+            Log.i("Main Activity", " OpenCV Loaded Successfully ");
+        } else {
+            Log.e("Main Activity", "OpenCV Initialization Faile: " );
+        }
+    }
+}
